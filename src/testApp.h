2887ba31cd4c919d972759ebae046e4270039e69
martin.froehlich <registered@maybites.ch>
2013-06-26 16:26:41 +0200

    firstCommit

---
+++ src/testApp.h
@@ -0,0 +1,62 @@
+#pragma once
+
+#include "ofMain.h"
+#include "ofxPCL.h"
+#include "ofxOpenCv.h"
+#include "ofxKinect.h"
+
+class testApp : public ofBaseApp
+{
+
+public:
+	void setup();
+	void update();
+	void draw();
+	void exit();
+
+    void drawPointCloud();
+
+	void keyPressed(int key);
+	void keyReleased(int key);
+	void mouseMoved(int x, int y);
+	void mouseDragged(int x, int y, int button);
+	void mousePressed(int x, int y, int button);
+	void mouseReleased(int x, int y, int button);
+	void windowResized(int w, int h);
+	void dragEvent(ofDragInfo dragInfo);
+	void gotMessage(ofMessage msg);
+	
+	ofEasyCam cam;
+	ofVboMesh mesh, meshraw;
+	bool dispRaw;
+    
+    //////////
+    //KINECT//
+    //////////
+    
+    ofxKinect kinect;
+	
+#ifdef USE_TWO_KINECTS
+	ofxKinect kinect2;
+#endif
+	
+	ofxCvColorImage colorImg;
+	
+	ofxCvGrayscaleImage grayImage; // grayscale depth image
+	ofxCvGrayscaleImage grayThreshNear; // the near thresholded image
+	ofxCvGrayscaleImage grayThreshFar; // the far thresholded image
+	
+	ofxCvContourFinder contourFinder;
+	
+	bool bThreshWithOpenCV;
+	bool bDrawPointCloud;
+	
+	int nearThreshold;
+	int farThreshold;
+	
+	int angle;
+	
+	// used for viewing the point cloud
+	ofEasyCam easyCam;
+
+};
